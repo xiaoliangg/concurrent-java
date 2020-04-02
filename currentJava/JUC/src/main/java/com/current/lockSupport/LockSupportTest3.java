7c4b41c68ae5c270da89b74ba2c6b9cdcf5b82c3
yuliang <yuliang@linksfield.net>
2020-04-02 16:42:04 +0800

    Default Changelist

--- currentJava/JUC/src/main/java/com/current/lockSupport/LockSupportTest3.java
+++ currentJava/JUC/src/main/java/com/current/lockSupport/LockSupportTest3.java
@@ -3,7 +3,7 @@ package com.current.lockSupport;
 import java.util.concurrent.locks.LockSupport;
 
 /**
- * 如果因为调用park而阻塞的话，能够响应中断请求(1.中断状态被设置成true,2.被park的线程可以继续向下执行)，但是不会抛出InterruptedException。
+ * 如果因为调用park而阻塞的话，能够响应中断请求(1.中断状态仍为true，不会被清除,2.被park的线程可以继续向下执行)，但是不会抛出InterruptedException。
  * https://blog.csdn.net/aitangyong/article/details/38373137
  */
 public class LockSupportTest3 {
