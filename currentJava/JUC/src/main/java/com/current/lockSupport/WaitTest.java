9355675f7544ac027032c2b5daa2bb255aff7370
yuliang <yuliang@ling.net>
2020-08-21 16:33:23 +0800

    wait让线程阻塞前，必须通过synchronized获取同步锁。

--- currentJava/JUC/src/main/java/com/current/lockSupport/WaitTest.java
+++ currentJava/JUC/src/main/java/com/current/lockSupport/WaitTest.java
@@ -1,7 +1,7 @@
 package com.current.lockSupport;
 
 /**
- * object.wait()IllegalMonitorStateException 演示
+ * object.wait()IllegalMonitorStateException 演示：wait让线程阻塞前，必须通过synchronized获取同步锁,否则抛异常
  */
 public class WaitTest {
     public static void main(String[] args) {
